e1f48e036dfb8f9c59beaac053f1c279bd187350
WSM <wsm21939@gmail.com>
2023-10-14 22:58:10 +0900

    kkkk

--- log/consumers.py
+++ log/consumers.py
@@ -1,33 +1,32 @@
+# log/consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class DataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         print("WebSocket connected!")
+        # Joining the "chat" group
+        await self.channel_layer.group_add("chat", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        # Leaving the "chat" group
+        await self.channel_layer.group_discard("chat", self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        self.message_content = data['data']  # 인스턴스 변수로 지정
+        message_content = data['data']
+
+        from .models import log_Message
+        message = log_Message(content=message_content)
+        message.save()
+
+        # No need to send a message to the group here because it's handled by the post_save signal.
 
     async def chat_message(self, event):
         message = event['message']
 
+        # Send a message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
-        }))
-
-        # Move the import here to avoid circular dependencies
-        from .models import log_Message
-
-        # 데이터베이스에 메시지 저장
-        message = log_Message(content=self.message_content)  # 인스턴스 변수로부터 값을 가져옴
-        message.save()
-
-        # 모든 클라이언트에게 메시지 전송
-        await self.send(text_data=json.dumps({
-            'data': self.message_content
+            'data': message
         }))
